071850c8d33a247e4ddf12ea967a53cea5734052
Olha Nahurna <linndfors@Olhas-MacBook-Pro.local>
2024-05-22 19:08:47 +0300

    add 2 tables

--- docker-create-table.sh
+++ docker-create-table.sh
@@ -1,2 +1,2 @@
-docker cp ./create-keyspace-table.cql homework12-cassandra-1:/create-keyspace-table.cql
-docker exec -it homework12-cassandra-1 cqlsh -f create-keyspace-table.cql
+docker cp ./create-keyspace-table.cql bigdata_wiki-cassandra-1:/create-keyspace-table.cql
+docker exec -it bigdata_wiki-cassandra-1 cqlsh -f create-keyspace-table.cql
